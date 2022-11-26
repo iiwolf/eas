25e596278da5d109bfcf251dc64cd360899c8bd6
Isaac Wolf <ijwolf8@gmail.com>
2022-11-26 01:09:16 -0600

    refactor: no Connection class, all components owned

--- src/app.rs
+++ src/app.rs
@@ -1,28 +1,29 @@
 use egui::{Vec2, Pos2, Color32};
 
-#[derive(serde::Deserialize, serde::Serialize)]
+#[derive(serde::Deserialize, serde::Serialize, Default, Debug)]
 pub struct Component{
     name: String,
-    pos: Pos2
+    pos: Pos2,
+    size: Vec2,
+    components: Vec<Component>
 }
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
 pub struct TemplateApp {
-    toggled: bool, 
     components: Vec<Component>,
-
 }
 
 impl Default for TemplateApp {
     fn default() -> Self {
+
+        let mut c1 = Component{name: "6DoF".to_string(), pos: Pos2{x: 600.0, y: 200.0}, size: Vec2{x: 100.0, y: 100.0}, ..Default::default()};
+        let mut c2 = Component{name: "Thermal".to_string(), pos: Pos2{x: 800.0, y: 200.0}, size: Vec2{x: 100.0, y: 100.0}, ..Default::default()};
+        c1.components.push(c2);
+        println!("{:?}", c1.components[0]);
         Self {
-            components: vec![
-                Component{name: "6DoF".to_string(), pos: Pos2{x: 600.0, y: 200.0}}, 
-                Component{name: "Thermal".to_string(), pos: Pos2{x: 800.0, y: 200.0}},
-            ],
-            toggled: true,
+            components: vec![c1],
         }
     }
 }
@@ -43,111 +44,77 @@ impl TemplateApp {
     }
 }
 
-pub fn toggle_ui(ui: &mut egui::Ui, on: &mut bool) -> egui::Response {
-    // Widget code can be broken up in four steps:
-    //  1. Decide a size for the widget
-    //  2. Allocate space for it
-    //  3. Handle interactions with the widget (if any)
-    //  4. Paint the widget
+// fn ui_connected_windows(ui: &mut egui::Ui, ctx: &egui::Context, components: &mut Vec<Component>) -> egui::Response {
+//     // Widget code can be broken up in four steps:
+//     //  1. Decide a size for the widget
+//     //  2. Allocate space for it
+//     //  3. Handle interactions with the widget (if any)
+//     //  4. Paint the widget
 
-    // 1. Deciding widget size:
-    // You can query the `ui` how much space is available,
-    // but in this example we have a fixed size widget based on the height of a standard button:
-    let desired_size = ui.spacing().interact_size.y * egui::vec2(8.0, 4.0);
-
-    // 2. Allocating space:
-    // This is where we get a region of the screen assigned.
-    // We also tell the Ui to sense clicks in the allocated region.
-    let (rect, mut response) = ui.allocate_exact_size(desired_size, egui::Sense::click());
-
-    // 3. Interact: Time to check for clicks!
-    if response.clicked() {
-        *on = !*on;
-        response.mark_changed(); // report back that the value changed
-    }
-
-    // Attach some meta-data to the response which can be used by screen readers:
-    response.widget_info(|| egui::WidgetInfo::selected(egui::WidgetType::Checkbox, *on, ""));
-
-    // 4. Paint!
-    // Make sure we need to paint:
-    if ui.is_rect_visible(rect) {
-        // Let's ask for a simple animation from egui.
-        // egui keeps track of changes in the boolean associated with the id and
-        // returns an animated value in the 0-1 range for how much "on" we are.
-        let how_on = ui.ctx().animate_bool(response.id, *on);
-        // We will follow the current style by asking
-        // "how should something that is being interacted with be painted?".
-        // This will, for instance, give us different colors when the widget is hovered or clicked.
-        let visuals = ui.style().interact_selectable(&response, *on);
-        // All coordinates are in absolute screen coordinates so we use `rect` to place the elements.
-        let rect = rect.expand(visuals.expansion);
-        let radius = 0.5 * rect.height();
-        ui.painter()
-            .rect(rect, radius, visuals.bg_fill, visuals.bg_stroke);
-        // Paint the circle, animating it from left to right with `how_on`:
-        let circle_x = egui::lerp((rect.left() + radius)..=(rect.right() - radius), how_on);
-        let center = egui::pos2(circle_x, rect.center().y);
-        ui.painter()
-            .circle(center, 0.75 * radius, visuals.bg_fill, visuals.fg_stroke);
-    }
-
-    // All done! Return the interaction response so the user can check what happened
-    // (hovered, clicked, ...) and maybe show a tooltip:
-    response
-}
-
-
-fn ui_connected_windows(ui: &mut egui::Ui, ctx: &egui::Context, components: &mut Vec<Component>) -> egui::Response {
-        // Widget code can be broken up in four steps:
-    //  1. Decide a size for the widget
-    //  2. Allocate space for it
-    //  3. Handle interactions with the widget (if any)
-    //  4. Paint the widget
-
-    // 1. Deciding widget size:
-    // You can query the `ui` how much space is available,
-    // but in this example we have a fixed size widget based on the height of a standard button:
-    let desired_size = egui::vec2(400.0, 400.0);
+//     // 1. Deciding widget size:
+//     // You can query the `ui` how much space is available,
+//     // but in this example we have a fixed size widget based on the height of a standard button:
+//     let desired_size = egui::vec2(400.0, 400.0);
     
-    // 2. Allocating space:
-    // This is where we get a region of the screen assigned.
-    // We also tell the Ui to sense clicks in the allocated region.
-    let (rect, mut response) = ui.allocate_exact_size(desired_size, egui::Sense::click());
+//     // 2. Allocating space:
+//     // This is where we get a region of the screen assigned.
+//     // We also tell the Ui to sense clicks in the allocated region.
+//     let (rect, mut response) = ui.allocate_exact_size(
+//         ui.available_size(),
+//         egui::Sense::click_and_drag()
+//     );
+//     // ui.allocate
+//     // 3. Interact: Time to check for clicks!
+//     if response.drag_started() {
 
-    // 3. Interact: Time to check for clicks!
-    if response.clicked() {
-        
-        response.mark_changed(); // report back that the value changed
-    }
+//         println!("Outside {:?}", response.drag_delta());
 
-    // Attach some meta-data to the response which can be used by screen readers:
-    // response.widget_info(|| egui::WidgetInfo::selected(egui::WidgetType::Checkbox, *on, ""));
+//     }
+//     if response.clicked() {
+//         println!("Clicked outside");
+//         response.mark_changed(); // report back that the value changed
+//     }
+
+//     // Attach some meta-data to the response which can be used by screen readers:
+//     // response.widget_info(|| egui::WidgetInfo::selected(egui::WidgetType::Checkbox, *on, ""));
     
-    egui::Window::new("6DoF")
-        .current_pos(components[0].pos)
-        .show(ctx, |ui| {
-            ui.label("6DoF");
-    });
+//     egui::Window::new("6DoF")
+//         .current_pos(components[0].pos)
+//         .show(ctx, |ui| {
+//             ui.label("6DoF");
+//             let (rect, mut response) = ui.allocate_exact_size(
+//                 ui.available_size(),
+//                 egui::Sense::click_and_drag()
+//             );
 
-    egui::Window::new("Thermal")
-    .current_pos(components[1].pos)
-    .show(ctx, |ui| {
-        ui.label("Thermal");
-    });
+//             if response.drag_started() {
+//                 println!("Inside {:?}", response.drag_delta());
 
-    let line_stroke = egui::Stroke{width: 1.0, color: egui::Color32::LIGHT_BLUE};
-    ui.painter().line_segment(
-        [components[0].pos, components[0].pos],
-        line_stroke
-    );
+//             }
+//             if response.clicked() {
+//                 println!("Clicked Inside");
+//                 response.mark_changed(); // report back that the value changed
+//             }
 
+//     });
 
-    // All done! Return the interaction response so the user can check what happened
-    // (hovered, clicked, ...) and maybe show a tooltip:
-    response
+//     egui::Window::new("Thermal")
+//     .current_pos(components[0].connections[0].pos)
+//     .show(ctx, |ui| {
+//         ui.label("Thermal");
+//     });
 
-}
+//     let line_stroke = egui::Stroke{width: 1.0, color: egui::Color32::LIGHT_BLUE};
+//     ui.painter().line_segment(
+//         [components[0].pos, components[0].connections[0].pos],
+//         line_stroke
+//     );
+
+//     // All done! Return the interaction response so the user can check what happened
+//     // (hovered, clicked, ...) and maybe show a tooltip:
+//     response
+
+// }
 
 impl eframe::App for TemplateApp {
     /// Called by the frame work to save state before shutdown.
@@ -158,7 +125,7 @@ impl eframe::App for TemplateApp {
     /// Called each time the UI needs repainting, which may be many times per second.
     /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        let Self { toggled, components} = self;
+        let Self {components} = self;
         
         // Examples of how to create different panels and windows.
         // Pick whichever suits you.
@@ -201,12 +168,43 @@ impl eframe::App for TemplateApp {
             ui.heading("Studio Floor");
             egui::warn_if_debug_build(ui);
 
-            ui_connected_windows(ui, ctx, components);
+            // ui_connected_windows(ui, ctx, components);
+            for component in components{
+                egui::Window::new(component.name.to_string())
+                    .current_pos(component.pos)
+                    .fixed_size(component.size)
+                    .show(ctx, |ui| {
+                    ui.label(component.name.to_string());
+                });
+                
+                // egui::Window::new(component.components[0].name.to_string())
+                //     .current_pos(component.components[0].pos)
+                //     .show(ctx, |ui| {
+                //     ui.label(component.components[0].name.to_string());
+                // });
 
-            // let mut toggled = true;
-             toggle_ui(ui, toggled);
-            
-            
+                for reference_component in &mut component.components{
+
+                    // Create component
+                    egui::Window::new(reference_component.name.to_string())
+                        .current_pos(reference_component.pos)
+                        .default_size(reference_component.size)
+                        .show(ctx, |ui| {
+                        ui.label(reference_component.name.to_string());
+                    });
+
+                    // Create connection
+                    let line_stroke = egui::Stroke{width: 1.0, color: egui::Color32::LIGHT_BLUE};
+                    ui.painter().line_segment(
+                        [
+                            component.pos + component.size / 2.0,
+                            reference_component.pos + component.size / 2.0],
+                        line_stroke
+                    );   
+                }
+
+            }
+
         });
         
 
