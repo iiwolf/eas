be6a069825fbac7b53e7e235efb4735d8c7ee667
Isaac Wolf <ijwolf8@gmail.com>
2022-11-27 09:03:13 -0800

    feat: functioning add component button!

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,7 @@
 use eframe::epaint::PathShape;
 use egui::{Vec2, Pos2, Color32, Ui, Stroke};
 
-#[derive(serde::Deserialize, serde::Serialize, Default, Debug)]
+#[derive(serde::Deserialize, serde::Serialize, Debug)]
 pub struct Component{
     name: String,
     pos: Pos2,
@@ -9,9 +9,21 @@ pub struct Component{
     components: Vec<Component>
 }
 
+impl Default for Component {
+    fn default() -> Self {
+        Self {
+            name: "Empty".to_string(),
+            pos: Pos2{x: 0.0, y: 0.0},
+            size: Vec2{x: 150.0, y: 150.0},
+            components: Vec::default()
+        }
+    }
+}
 impl Component{
+
     fn create_window(&mut self, ctx: &egui::Context) {
         egui::Window::new(self.name.to_string())
+            .title_bar(false)
             .fixed_size(self.size)
             .default_pos(self.pos)
             .current_pos(self.pos)
@@ -28,31 +40,13 @@ impl Component{
         });
     }
 
-    fn create_child_window(&mut self, ctx: &egui::Context, ui: &mut Ui) {
-
+    fn create_child_window(&mut self, ctx: &egui::Context) {
         self.create_window(ctx);
-
     }
 
     fn create_globdule(&mut self, ctx: &egui::Context, ui: &mut Ui) {
         
         let stroke = egui::Stroke{width: 1.0, color: egui::Color32::WHITE};
-        // let mesh = egui::Mesh{
-        //     // indices: vec![0, 1, 2],
-        //     // vertices: vec![
-        //     //     egui::epaint::Vertex{pos: self.pos, ..Default::default()},
-        //     //     egui::epaint::Vertex{pos: self.pos + self.size / 2.0, ..Default::default()},
-        //     //     egui::epaint::Vertex{pos: self.pos - self.size / 2.0, ..Default::default()},
-        //     // ], 
-        //     // texture_id: ctx
-        //     ..Default::default()
-        // };
-        // let size = 100.0;
-        // ui.painter().line_segment([self.pos, Pos2{x: self.pos[0] + size, y: self.pos[1]}], egui::Stroke{width: 1.0, color: egui::Color32::WHITE});
-        // ui.painter().line_segment([self.pos, Pos2{x: self.pos[0], y: self.pos[1]  + size}], egui::Stroke{width: 1.0, color: egui::Color32::LIGHT_BLUE});
-        // ui.painter().line_segment([Pos2{x: self.pos[0] + size, y: self.pos[1]}, Pos2{x: self.pos[0], y: self.pos[1]  + size}], egui::Stroke{width: 1.0, color: egui::Color32::RED});
-        // ui.painter().line_segment([self.pos, self.pos - self.size / 2.0], egui::Stroke{width: 1.0, color: egui::Color32::RED});
-        // ui.painter().line_segment([self.pos + self.size / 2.0, self.pos - self.size / 2.0], egui::Stroke{width: 1.0, color: egui::Color32::LIGHT_BLUE});
         let size = self.size[0];
         let delta = size * 3.0_f32.powf(0.5) / 3.0;
         let gamma = 30.0_f32.to_radians().tan() * size / 2.0;
@@ -63,18 +57,6 @@ impl Component{
             Pos2{x: self.pos[0] - 0.5 * size, y: self.pos[1] - gamma}, 
         ];
         ui.painter().add(PathShape::convex_polygon(points, Color32::GRAY, stroke));
-
-        // for component in &mut self.components{
-
-        //     // let origin = self.pos + Pos2 { x: gamma, y: gamma }.to_vec2();
-        //     let points = vec![
-        //         Pos2{x: component.pos[0], y: component.pos[1] + delta}, 
-        //         Pos2{x: component.pos[0] + 0.5 * size, y: component.pos[1] - gamma}, 
-        //         Pos2{x: component.pos[0] - 0.5 * size, y: component.pos[1] - gamma}, 
-        //     ];
-        // ui.painter().add(PathShape::convex_polygon(points, Color32::GRAY, stroke));
-
-        // }
 
         let mut origin = Pos2{x: self.pos[0] + gamma, y: self.pos[1]};
         for i in 1..10 {
@@ -114,8 +96,8 @@ pub struct TemplateApp {
 impl Default for TemplateApp {
     fn default() -> Self {
 
-        let mut c1 = Component{name: "6DoF".to_string(), pos: Pos2{x: 600.0, y: 200.0}, size: Vec2{x: 100.0, y: 100.0}, ..Default::default()};
-        let mut c2 = Component{name: "Thermal".to_string(), pos: Pos2{x: 800.0, y: 200.0}, size: Vec2{x: 100.0, y: 100.0}, ..Default::default()};
+        let mut c1 = Component{name: "6DoF".to_string(), pos: Pos2{x: 600.0, y: 200.0}, size: Vec2{x: 150.0, y: 150.0}, ..Default::default()};
+        let mut c2 = Component{name: "Thermal".to_string(), pos: Pos2{x: 800.0, y: 200.0}, size: Vec2{x: 150.0, y: 150.0}, ..Default::default()};
         c1.components.push(c2);
         println!("{:?}", c1.components[0]);
         Self {
@@ -224,13 +206,24 @@ impl eframe::App for TemplateApp {
             ui.heading("Studio Floor");
             egui::warn_if_debug_build(ui);
             draw_grid(ui, egui::Stroke{width: 1.0, color: egui::Color32::from_gray(60)});
-            // ui.painter().line_segment([margin, (size - margin).to_pos2()], grid_stroke);
+            let parent_rect = ui.min_rect().right_bottom();
+            let pos = parent_rect - Pos2{x:100.0, y:100.0}.to_vec2();
+            let text = egui::RichText::new("Add Component").font(egui::FontId::proportional(40.0));
+            egui::Area::new("add_button_area")
+                .anchor(egui::Align2::RIGHT_BOTTOM, Vec2{x:-100.0, y:-100.0})
+                .show(ctx, |ui| {
+                    if ui.button(text).clicked() {
+                        components.push(Component::default());
+                    }
+                }
+            );
+
             // ui_connected_windows(ui, ctx, components);
             for component in components{
-                // component.create_window(ctx);
-                component.create_globdule(ctx, ui);
+                component.create_window(ctx);
+
                 for child_component in &mut component.components{
-                    child_component.create_child_window(ctx, ui);
+                    child_component.create_window(ctx);
 
                     // Create connection
                     let line_stroke = egui::Stroke{width: 1.0, color: egui::Color32::LIGHT_BLUE};
@@ -244,8 +237,11 @@ impl eframe::App for TemplateApp {
                 }
 
             }
+            
+
 
         });
+        
         
 
         if false {
